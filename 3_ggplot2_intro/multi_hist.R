7be06bce3c29fa9ca38d91a44e8b72a9d1b8053d
hujuu <out.of.the.base.sfc@gmail.com>
2016-09-11 11:04:27 +0900

    文字化け回避のため、注釈を英語にしました

--- 3_ggplot2_intro/multi_hist.R
+++ 3_ggplot2_intro/multi_hist.R
@@ -3,29 +3,29 @@ install.packages("plyr")
 library(ggplot2)
 library(plyr) # load revalue()
 
-data <- read.csv(file("r-test/3_ggplot2_intro/office.csv",encoding='Shift_JIS'))
-data
+data <- read.csv(file("r-test/3_ggplot2_intro/office.csv",
+                      encoding='Shift_JIS'))
 
 ggplot(data, aes(x = office)) +
   geom_histogram()
 
-# 複数のヒストグラムを別々に描画する
+# Draw separately the two histograms
 ggplot(data, aes(x = office)) +
   geom_histogram(binwidth = 150, fill = "white", color = "black") +
   facet_grid(division ~ .)
 
-# divisionをファクタに変換する
+# To convert to factor
 data$division <- factor(data$division)
 levels(data$division)
 
-# ファクターに名称を付ける
+# Name the factor
 data$division <- revalue(data$division, c("0"="Wholesale", "1"="retail"))
 
-# 複数のヒストグラムを重ねて描画する
+# To draw over multiple histogram
 ggplot(data, aes(x = office, fill = division)) +
   geom_histogram(binwidth = 150, position = "identity", alpha = 0.4)
 
-# 余録：デザインテーマの変更
+# ex:design theme changes
 install.packages("ggthemes")
 require(ggthemes)
 g <- ggplot(data, aes(x = office, fill = division)) +
